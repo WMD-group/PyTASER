3de13d8705ad50c7afedce15b9eaac80a492cbf3
Savya Aggarwal <44528580+savya10@users.noreply.github.com>
2021-12-19 14:05:21 +0000

    remove abipy dependencies

--- examples/Fe2O3/Fe2O3.py
+++ examples/Fe2O3/Fe2O3.py
@@ -7,13 +7,7 @@ import plotter
 import numpy as np
 
 #abipy
-import abipy.data as abidata
-import abipy.abilab as abilab
-import abipy.electrons as abielectrons
-from abipy.abilab import abiopen
 from abipy.tools.numtools import gaussian
-abilab.enable_notebook()
-from abipy.abilab import AbinitInput
 
 #collections
 from collections import OrderedDict
@@ -29,11 +23,10 @@ import scipy.constants as scpc
 
 #######################
 
-fe2o3_temp = 298
-fe2o3_conc = 10e21
+fe2o3_temp = 298 
+fe2o3_conc = 10e21 
 bandgap = 2.2 #based on https://aip.scitation.org/doi/full/10.1063/1.2177426
 transitions_fe2o3 = [(8,11, 'spin = down'), (9,10, 'spin = up'), (4,14, 'spin = up'), (6,20, 'spin = down'), (6,28, 'spin = up')]
-
 # Fe2O3 is  spin-polarised, so we must define whether the relevant transitions we want to see are for spin-up or spin-down
 
 fe2o3_generator = generator.TASGenerator.from_mpid('cTHyvyRHqX34gOLMbM', 'mp-565814', fe2o3_temp, fe2o3_conc, bandgap)
