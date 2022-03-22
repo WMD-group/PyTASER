0d3309f2ce3ea6d3359e263f526028c154031a75
savya10 <sa13018@ic.ac.uk>
2022-03-22 09:12:36 +0000

    BiOI lambda and ev plots

---
+++ examples/BiOI/BiOI.py
@@ -0,0 +1,18 @@
+from pytaser import generator, plotter
+
+api_key = None  # Use your API key here
+bioi_temp = 298
+bioi_conc = 10e21
+bandgap = 1.94
+
+bioi_generator = generator.TASGenerator.from_mpid(
+    "mp-22987", bioi_temp, bioi_conc, bandgap, api_key=api_key
+)
+bioi_tas = bioi_generator.generate_tas(energy_min=0.0, energy_max=10.0)
+bioi_plotter = plotter.TASPlotter(
+    bioi_tas, bandgap, material_name="BiOI", temp=bioi_temp, conc=bioi_conc
+)
+plot_bioi = bioi_plotter.get_plot(
+    xaxis="wavelength", transition_cutoff=0.03, xmin=350, xmax=1400, yaxis="TAS (deltaT)"
+)
+plot_bioi.show()
