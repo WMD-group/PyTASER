abe13b1bd43928fff607ddd93046d0421348ee95
savya10 <sa13018@ic.ac.uk>
2023-03-14 00:58:44 +0000

    setup and requirements

--- setup.py
+++ setup.py
@@ -38,19 +38,31 @@ setup(
         "scipy",
         "matplotlib",
         "pymatgen>=2017.12.30",
+        "setuptools"
     ],
     extras_require={
         "tests": [
             "pytest>=7.1.3",
             "pytest-mpl==0.15.1",
-        ]
+            "deepdiff",
+            "monty",
+
+        ],
         "docs": [
-            "sphinx"
-            "sphinx-book-theme"
-            "sphinx-rtd-theme"
-            "sphinx_click"
-            "sphinx_design"
-        ]
-    }
+            "sphinx",
+            "sphinx-book-theme",
+            "sphinx-rtd-theme",
+            "sphinx_click",
+            "sphinx_design",
+            "nbsphinx",
+            "nbsphinx_link",
+            "sphinx_copybutton",
+            "sphinx_toggleprompt",
+            "sphinx-autobuild",
+            "sphinx_minipres",
+            "sphinx_tabs",
+            "sphinx_togglebutton"
+        ],
+    },
     data_files=["LICENSE"],
 )
