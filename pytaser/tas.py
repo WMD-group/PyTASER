4e79b14accd7cdf19bd28df3cc8d6bd176d27d3f
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-05-12 13:24:03 +0100

    Add `as_dict()` and `from_dict()` methods to `Tas` object, to easily store reloadable data in `json` format (useful as WAVEDER parsing can take a small while)

--- pytaser/tas.py
+++ pytaser/tas.py
@@ -1,3 +1,6 @@
+import ast
+from monty.json import MontyDecoder
+
 class Tas:
     """
     A container class for the data from TASgenerator.
@@ -36,4 +39,77 @@ class Tas:
         self.jdos_dark_tot = jdos_dark_total
         self.jdos_dark_decomp = jdos_dark_decomp
         self.energy_mesh_ev = energy_mesh_ev
-        self.bandgap = bandgap_ev
+        self.bandgap = bandgap
+        self.temp = temp
+        self.conc = conc
+        self.alpha_dark = alpha_dark
+        self.alpha_light_dict = alpha_light_dict
+        self.weighted_jdos_light_if = weighted_jdos_light_if
+        self.weighted_jdos_dark_if = weighted_jdos_dark_if
+        self.weighted_jdos_diff_if = weighted_jdos_diff_if
+
+    def as_dict(self):
+        """
+        JSON-serializable dict representation of Tas
+        """
+        json_dict = {
+            "@module": type(self).__module__,
+            "@class": type(self).__name__,
+            "tas_total": self.tas_total,
+            "jdos_diff_if": self.jdos_diff_if,
+            "jdos_light_total": self.jdos_light_total,
+            "jdos_light_if": self.jdos_light_if,
+            "jdos_dark_total": self.jdos_dark_total,
+            "jdos_dark_if": self.jdos_dark_if,
+            "energy_mesh_ev": self.energy_mesh_ev,
+            "bandgap": self.bandgap,
+            "temp": self.temp,
+            "conc": self.conc,
+            "alpha_dark": self.alpha_dark,
+            "alpha_light_dict": self.alpha_light_dict,
+            "weighted_jdos_light_if": self.weighted_jdos_light_if,
+            "weighted_jdos_dark_if": self.weighted_jdos_dark_if,
+            "weighted_jdos_diff_if": self.weighted_jdos_diff_if,
+        }
+        for key, value in json_dict.items():
+            if isinstance(value, dict):
+                json_dict[key] = {
+                    str(k): v for k, v in value.items()
+                }  # decomp dicts, can't have tuples as keys
+        return json_dict
+
+    @classmethod
+    def from_dict(cls, d):
+        """
+        Reconstructs Tas object from a dict representation of Tas created using
+        Tas.as_dict().
+
+        Args:
+            d (dict): dict representation of Tas
+
+        Returns:
+            Tas object
+        """
+
+        def convert_to_tuple(subdict):
+            if isinstance(subdict, dict) and "@module" not in subdict:
+                return {
+                    ast.literal_eval(k) if "(" in k and ")" in k else k: v
+                    for k, v in subdict.items()
+                }
+            return subdict
+
+        def decode_dict(subdict):
+            if isinstance(subdict, dict) and "@module" in subdict:
+                return MontyDecoder().process_decoded(subdict)
+
+            elif isinstance(subdict, dict) and "@module" not in subdict:
+                for k, v in subdict.items():
+                    if isinstance(v, dict) and "@module" in v:
+                        subdict[k] = MontyDecoder().process_decoded(v)
+
+            return subdict
+
+        d_dec = {k: convert_to_tuple(v) for k, v in d.items()}
+        d_decoded = {k: decode_dict(v) for k, v in d_dec.items()}
+        return cls(**d_decoded)
